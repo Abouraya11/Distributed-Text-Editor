da8c7b3fd20f593bdf542a9406f2a5f408da229e
Abouraya11 <youssefabouraya7@gmail.com>
2022-05-01 20:13:34 +0200

    Remove express from mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,10 @@ const port = 4000;
 
 const dbURI =
   "mongodb://dist:dist123@cluster0-shard-00-00.aldhx.mongodb.net:27017,cluster0-shard-00-01.aldhx.mongodb.net:27017,cluster0-shard-00-02.aldhx.mongodb.net:27017/files?ssl=true&replicaSet=atlas-rtgjl1-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(port))
-  .catch((err) => console.log(err));
+  mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
   const socket_io = require("socket.io")(4000, {
     origin: "http://localhost:3000",
