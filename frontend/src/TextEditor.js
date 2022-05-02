5ee04a46b53b565aee5d38a0c3cfad26169cf51d
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2022-05-02 22:41:14 +0200

    Setting sockets and connect to server in Frontend

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -1,4 +1,21 @@
+import { io } from "socket.io-client";
+import React, { useEffect,useState } from "react";
+import { useParams } from "react-router-dom"; // React DOM Version 5
+
 function TextEditor() {
+
+  const { id: doc_id } = useParams();
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    const socket_io = io("http://localhost:4000");
+    setSocket(socket_io);
+    return () => {
+      socket_io.disconnect();
+    };
+  }, []);
+
+
   return <div>Hi</div>;
 }
 
