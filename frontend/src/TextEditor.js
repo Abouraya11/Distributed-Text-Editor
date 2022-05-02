f2e5ee7658f7c4d093cfeafcc43604b883cd4cb2
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2022-05-02 22:42:08 +0200

    Resolve cors credentials

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -1,20 +1,22 @@
 import { io } from "socket.io-client";
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"; // React DOM Version 5
 
 function TextEditor() {
-
   const { id: doc_id } = useParams();
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const socket_io = io("http://localhost:4000");
+    // to resolve cors credentials problem (access-control-allow-origin)
+    // Request to server of port 4000
+    const socket_io = io("http://localhost:4000", {
+      transports: ["websocket", "polling", "flashsocket"],
+    });
     setSocket(socket_io);
     return () => {
       socket_io.disconnect();
     };
   }, []);
-
 
   return <div>Hi</div>;
 }
