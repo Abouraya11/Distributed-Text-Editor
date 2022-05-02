70b186dd32d24c50ce3160a501f53cc451d2a920
Abouraya11 <youssefabouraya7@gmail.com>
2022-05-02 23:22:05 +0200

    Adding the wrapper handler in a call back which has the quill init to show the UI of the text editor

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client";
-import React, { useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import React, { useEffect, useState,useCallback } from "react";
 import { useParams } from "react-router-dom"; // React DOM Version 5
 
 function TextEditor() {
   const { id: doc_id } = useParams();
   const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
   useEffect(() => {
     // to resolve cors credentials problem (access-control-allow-origin)
@@ -18,7 +21,26 @@ function TextEditor() {
     };
   }, []);
 
-  return <div>Hi</div>;
+  const wrapper_handler = useCallback((wrapper) => {
+
+    const container = document.createElement("div");
+
+    wrapper.append(container);
+
+    const q = new Quill(container, { theme: "snow" });
+
+    wrapper.innerHtml = "";
+
+    setQuill(q);
+  }, []);
+
+  
+  return (
+    <div>
+      <div id="container" ref={wrapper_handler}></div>
+    </div>
+  );
 }
+
 
 export default TextEditor;
